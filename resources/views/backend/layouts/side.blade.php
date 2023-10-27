2db7067fc50b41d531fa2e2ed5a6d8de55f18c9e
H@n <hn@Hns-MacBook-Pro.local>
2023-10-27 16:32:34 +0630

    project install

---
+++ resources/views/backend/layouts/side.blade.php
@@ -0,0 +1,36 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar">
+				<div class="sidebar-brand">
+                    <span class="align-middle">Job Portal</span>
+                </div>
+
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
+					<li class="sidebar-item @yield('home')">
+						<a class="sidebar-link" href="{{ route('admin.home')}}">
+                          <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                         </a>
+					</li>
+
+					<li class="sidebar-item @yield('admin_user')">
+						<a class="sidebar-link" href="#">
+                            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Admin User</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item @yield('user')">
+						<a class="sidebar-link" href="#">
+                            <i class="align-middle" data-feather="user"></i> <span class="align-middle">User</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item @yield('wallet')">
+						<a class="sidebar-link" href="#">
+                            <i class="align-middle" data-feather="credit-card"></i> <span class="align-middle">Wallet</span>
+                        </a>
+					</li>
+				</ul>
+			</div>
+</nav>
